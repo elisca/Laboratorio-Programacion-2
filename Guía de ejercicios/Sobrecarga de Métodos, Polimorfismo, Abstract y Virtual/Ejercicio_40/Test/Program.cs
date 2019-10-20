bf983ae92a2038a3d507189ec649c9e0ee1be8b7
Ezequiel Lisca <ezequiel.lisca@live.com>
2019-10-20 16:33:06 -0300

    Ejercicio 40 incompleto
    - Corregir el Main para agregar llamadas
    - Implementar propiedad abstracta
    - Hacer formulario

---
+++ Guía de ejercicios/Sobrecarga de Métodos, Polimorfismo, Abstract y Virtual/Ejercicio_40/Test/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CentralitaHerencia;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mi central
+            Centralita c = new Centralita("Fede Center");
+            
+            // Mis 4 llamadas
+            Local l1 = new Local("Bernal", 30, "Rosario", 2.65f);
+            Provincial l2 = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal");
+            Local l3 = new Local("Lanús", 45, "San Rafael", 1.99f);
+            Provincial l4 = new Provincial(Provincial.Franja.Franja_3, l2);
+            
+            // Las llamadas se irán registrando en la Centralita.
+            // La centralita mostrará por pantalla todas las llamadas según las vaya registrando.
+            c = c + l1;
+            c = c + l2;
+            c = c + l3;
+            c = c + l4;
+            c.OrdenarLlamadas();
+            Console.WriteLine(c.ToString());
+            
+            Console.ReadKey();    
+        }
+    }
+}
